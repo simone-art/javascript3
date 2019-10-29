6d771afda9a0c785b825673e2bb253588cc0d34e
simone-art <simoneperiodista3@gmail.com>
2019-10-29 16:16:31 -0300

    realizado el ejercicio ajax 1d con imágenes de gatos. El código funciona perfectamente

---
+++ 01d_ajax_giphy_random/index.js
@@ -0,0 +1,40 @@
+// fetch da imagem
+// [X] adicionar a tag <script> no html
+// [X] verificar se está lendo index.js
+// [] usar o método fetch() para a url da API Giphy
+// [] transformar o dado recebido
+// [] apresentar <img>
+// [] usar async/await, then(), .catch(), arrow function
+//XMLHttpRequest do gif 
+
+const request = new XMLHttpRequest()
+// const url = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho'
+const url = 'http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC&limit=1&offset=0'
+request.onreadystatechange = mostraGif;
+request.open('GET', url);
+request.send()
+
+function mostraGif(){
+    if (request.readyState === 4 && request.status === 200) {
+        const response = request.response;
+//         // console.log(typeof response)
+        const json = JSON.parse(response);
+        // console.log(json.data.images.original.url);
+//         let imagem = json.data.images.original.url;
+//         let acessar = document.getElementById("imagem");
+//         acessar.setAttribute("src", imagem);
+
+        const imgSrc = json.data[0].images.original.url
+        document.querySelector('#imagem').src = imgSrc;
+
+    } else {
+
+    }
+
+}
+
+
+
+
+
+
