e362f588379be82e0d04ec7637b9cde1c9407d60
simone-art <simoneperiodista3@gmail.com>
2019-11-01 16:47:34 -0300

    realizado el mismo ejercicio para ver mi avatar del github. En el código está el paso a paso y funciona perfectamente

--- github_api/js/index.js
+++ github_api/js/index.js
@@ -1,31 +1,46 @@
 
-const url = `http://api.giphy.com/v/gifs/search?q=beyonce&api_key=dc6zaTOxFJmzC&limit=1&offset=0`
+// const url = `http://api.giphy.com/v/gifs/search?q=beyonce&api_key=dc6zaTOxFJmzC&limit=1&offset=0`
 
-'https://api.github.com/users/simone-art'
+const url = 'https://api.github.com/users/simone-art'
 
 async function buscarApi(url) {
-    const response = await fetch(url)
-    if (response.ok) {
-        const json = await response.json();
-        criarImagem(json.data)
+    try {
+        const response = await fetch(url)
+        if (response.ok) {
+            const json = await response.json();
+            console.log(json)
+            criarImagem(json.avatar_url);
 
-    } else {
-        //USA ESTOS DOS COMANDO A FIN DE ENCONTRAR UN ERROR//
-        console.error("nao foi possível receber o resultado da api");  
-        document.body.innerHTML = `<h1 style="color: blue">${response.status}:${response.statusText}</h1>`
+
+        } else {
+            //USA ESTOS DOS COMANDO PARA INDICAR QUE HAY UN ERROR EN LA REQUISICIÓN DEL API//
+            // console.error("nao foi possível receber o resultado da api");  
+            const err = `${response.status}:${response.statusText}`
+            criaErro(err);
+            // document.body.innerHTML = `<h1 style="color: blue">${response.status}:${response.statusText}</h1>`
+        }
+
+    } catch (error) {
+        criaErro(`falha na requisiçao de API`)
     }
-        // .then(response => response.json())
-        // .then(json => criarImagem(json.data))
-        // .catch(erro => console.error(erro))
-
 }
 
 
-    function criarImagem(lista) {
-        console.log(lista);
-        document.body.innerHTML = `
-        <img src =${lista[0].images.original.url}>
+
+// .then(response => response.json())
+// .then(json => criarImagem(json.data))
+// .catch(erro => console.error(erro))
+
+
+function criarImagem(src) {
+    // console.log(lista);
+    document.body.innerHTML = `
+        <img src =${src}>
         `
 }
 
-buscarApi(url).catch(err => console.error(err))
+criaErro = (erro) => {
+    document.body.innerHTML = `<h1 style="color: blue">${erro}</h1>`
+}
+
+buscarApi(url)
